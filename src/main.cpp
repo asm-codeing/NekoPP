70aa42d1bd712268410787c2a8a0a2708b65b3ff
BackGwa <backgwa@icloud.com>
2023-11-30 17:26:43 +0900

    인자 입력 테스트

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
-// std 네임스페이스 사용
+/* std 네임스페이스 사용 */
 using namespace std;
 
-// 프로그램 시작점
-int main() {
-    cout << "TestCode" << endl;
-    return 0;
+/* 전역 변수 */
+string filepath;
+
+/* main : Neko++의 인터프리터의 시작점입니다. */
+int main(int argc, char *argv[]) {
+  if (argc > 1) {
+    filepath = (string)argv[1];
+    cout << filepath << endl;
+  } else {
+    cout << "입력 파일이 설정되지 않았습니다." << endl;
+  }
+
+  return 0;
 }
